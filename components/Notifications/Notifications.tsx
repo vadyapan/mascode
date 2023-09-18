6bd02742b5f8e095b0d330080be57f71f228f900
Vlad Pantyukhin <pantyukhinvlad@gmail.com>
2023-09-18 16:22:03 +0300

    deleted module css

--- components/Notifications/Notifications.tsx
+++ components/Notifications/Notifications.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { notifications } from '@mantine/notifications';
 import { IconCheck, IconX } from '@tabler/icons-react';
-import './Notifications.module.css';
 
 export const successNotification = (): void => {
   notifications.show({
