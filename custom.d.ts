d9128e703a25865f125aa86c6a48d263dfd15543
Vlad Pantyukhin <pantyukhinvlad@gmail.com>
2023-09-21 10:24:00 +0300

    fixed config

--- custom.d.ts
+++ custom.d.ts
@@ -1,4 +1,8 @@
 declare module '*.svg' {
-  const content: any;
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
 }
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
